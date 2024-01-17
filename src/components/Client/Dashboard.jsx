f49ceaeb48ce96a5c85e8d904aae967fbfb64330
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-17 20:33:44 +0300

    add client avatar

--- src/components/Client/Dashboard.jsx
+++ src/components/Client/Dashboard.jsx
@@ -5,11 +5,21 @@ function Dashboard() {
   const { user } = useUser();
   return (
     <div className={styles["dashboard-container"]}>
-      <h1 className={styles["client-intro"]}>Welcome {user?.username}</h1>
-      <p className={styles["intro-description"]}>
-        We guarante the best experience in Lounge reservation. Explore Rooms at
-        your convenience at the spaces tab
-      </p>
+      <div className={styles["client-intro-section"]}>
+        <h1 className={styles["client-intro"]}>Welcome {user?.username}</h1>
+        <p className={styles["intro-description"]}>
+          We guarante the best experience in Lounge reservation. Explore Rooms
+          at your convenience at the spaces tab
+        </p>
+      </div>
+
+      <div className={styles["client-profile"]}>
+        <img
+          src="https://i.pravatar.cc/100?u=zz"
+          alt="client profile"
+          className={styles["client-avatar"]}
+        />
+      </div>
     </div>
   );
 }
