da799c6b1feb11ea1b30cb797ce7d5a9be6a9bdc
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-17 20:26:04 +0300

    add padding to dashboard

--- src/components/Client/Dashboard.jsx
+++ src/components/Client/Dashboard.jsx
@@ -6,6 +6,10 @@ function Dashboard() {
   return (
     <div className={styles["dashboard-container"]}>
       <h1 className={styles["client-intro"]}>Welcome {user?.username}</h1>
+      <p className={styles["intro-description"]}>
+        We guarante the best experience in Lounge reservation. Explore Rooms at
+        your convenience at the spaces tab
+      </p>
     </div>
   );
 }
