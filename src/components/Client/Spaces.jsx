62c23f5fd11a02a76d5eb7fd9d2b098a056bd548
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-18 22:48:35 +0300

    add useReducer to managestate

--- src/components/Client/Spaces.jsx
+++ src/components/Client/Spaces.jsx
@@ -1,4 +1,36 @@
+import { useEffect, useReducer } from "react";
+import { baseurl } from "../../utils";
+
+const initialState = {
+  spaces: [],
+  status: "loading",
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+  }
+}
+
 function Spaces() {
+  const [{ spaces, status, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  useEffect(() => {
+    async function fetchSpaces() {
+      try {
+        const res = await fetch(`${baseurl}/spaces`);
+        const data = await res.json();
+        dispatch({ type: "spaces/loaded", payload: data });
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchSpaces();
+  }, []);
   return <div>Spaces</div>;
 }
 
