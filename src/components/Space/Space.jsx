d83d68ad9f62994b54595462b805f7e83405e0d3
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-21 17:40:02 +0300

    add bounce loader

--- src/components/Space/Space.jsx
+++ src/components/Space/Space.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useSpace } from "../../context/SpaceContext";
 import { useEffect } from "react";
+import { BounceLoader, DotLoader } from "react-spinners";
 
 function Space() {
   const { getSpace, currentSpace, isLoading } = useSpace();
+  const { name, description, location, price, contact, image } = currentSpace;
   const { id } = useParams();
 
   useEffect(() => {
@@ -12,7 +14,23 @@ function Space() {
 
   console.log(currentSpace);
 
-  return <div>Space {id}</div>;
+  if (isLoading)
+    return (
+      <div>
+        <BounceLoader />
+      </div>
+    );
+
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{location}</p>
+      <p>{price}</p>
+      <p>{contact}</p>
+      <img src={image} alt={name} />
+    </div>
+  );
 }
 
 export default Space;
