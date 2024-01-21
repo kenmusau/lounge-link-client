04033284310700cab45ccac7fca3a52f179718b2
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-21 18:00:33 +0300

    add back button to spaces

--- src/components/Space/Space.jsx
+++ src/components/Space/Space.jsx
@@ -1,12 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useSpace } from "../../context/SpaceContext";
 import { useEffect } from "react";
-import { BounceLoader, DotLoader } from "react-spinners";
+import { BounceLoader } from "react-spinners";
+
+import styles from "./Space.module.css";
 
 function Space() {
   const { getSpace, currentSpace, isLoading } = useSpace();
   const { name, description, location, price, contact, image } = currentSpace;
+
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getSpace(id);
@@ -21,14 +25,22 @@ function Space() {
       </div>
     );
 
+  function handleBackButton(e) {
+    e.preventDefault();
+    navigate(-1);
+  }
+
   return (
-    <div>
-      <p>{name}</p>
-      <p>{description}</p>
-      <p>{location}</p>
-      <p>{price}</p>
-      <p>{contact}</p>
-      <img src={image} alt={name} />
+    <div className={styles["space-container"]}>
+      <div className={styles.space}>
+        <button onClick={handleBackButton}>&larr; Back</button>
+        <p>{name}</p>
+        <p>{description}</p>
+        <p>{location}</p>
+        <p>{price}</p>
+        <p>{contact}</p>
+        <img src={image} alt={name} />
+      </div>
     </div>
   );
 }
