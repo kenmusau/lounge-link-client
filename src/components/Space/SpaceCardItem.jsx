35b358612eaa0f3a0321980705334b154f218985
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-21 10:20:46 +0300

    add space context

--- src/components/Space/SpaceCardItem.jsx
+++ src/components/Space/SpaceCardItem.jsx
@@ -6,7 +6,7 @@ function SpaceCardItem({ space }) {
 
   return (
     <div className={styles["spaceCard-container"]}>
-      <Link to={`${space.id}`} className={styles.card}>
+      <Link to={`${space.id}?hse=${space.name}`} className={styles.card}>
         <img className={styles["card-image"]} src={space.image} alt="" />
         <div className={styles["card-content"]}>
           <h3>{space.name}</h3>
