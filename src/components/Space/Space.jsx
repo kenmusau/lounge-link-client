884abb1075740d82940f08d705cf5ac88d7d4acf
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-23 22:58:45 +0300

    flex features in space

--- src/components/Space/Space.jsx
+++ src/components/Space/Space.jsx
@@ -36,56 +36,58 @@ function Space() {
   return (
     <div className={styles["space-container"]}>
       <div className={styles.space}>
-        <button
-          onClick={handleBackButton}
-          className={`${styles.btn} ${styles["back--btn"]}`}
-        >
-          &larr; Back
-        </button>
-
         <div>
-          <img src={image} alt={name} />
-          <h3 className={styles["space-title"]}>{name}</h3>
+          <button
+            onClick={handleBackButton}
+            className={`${styles.btn} ${styles["back--btn"]}`}
+          >
+            &larr; Back
+          </button>
 
-          <p className={styles.description}>
-            <span>Description: </span> {description}
-          </p>
+          <div>
+            <img src={image} alt={name} />
+            <h3 className={styles["space-title"]}>{name}</h3>
 
-          <p className={styles["phone-number"]}>
-            <span>Phone Number:</span> {contact}
-          </p>
+            <p className={styles.description}>
+              <span>Description: </span> {description}
+            </p>
 
-          <div className={styles["location-price"]}>
-            <div className={styles["icons"]}>
-              <HiOutlineLocationMarker />
-              <p>{location}</p>
+            <p className={styles["phone-number"]}>
+              <span>Phone Number:</span> {contact}
+            </p>
+
+            <div className={styles["location-price"]}>
+              <div className={styles["icons"]}>
+                <HiOutlineLocationMarker />
+                <p>{location}</p>
+              </div>
+
+              <div className={styles["icons"]}>
+                <HiOutlineCash />
+                <p>{price}</p>
+              </div>
             </div>
 
-            <div className={styles["icons"]}>
-              <HiOutlineCash />
-              <p>{price}</p>
+            <div className={styles["act-btns"]}>
+              <button className={styles["btn"]}>Available</button>
+              <button className={styles["btn"]}>Book Now</button>
             </div>
-          </div>
-
-          <div className={styles["act-btns"]}>
-            <button className={styles["btn"]}>Available</button>
-            <button className={styles["btn"]}>Book Now</button>
           </div>
         </div>
-      </div>
 
-      <div>
-        <h2>Features</h2>
-        <ul>
-          <li>– Fine dining restaurants </li>
-          <li>– Casual dining options</li>
-          <li>– Buffet or breakfast areas</li>
-          <li>– Rooftop bars and lounges</li>
-          <li>– Childcare or babysitting services</li>
-          <li>– Kids’ clubs and play areas</li>
-          <li>– Family-friendly room options</li>
-          <li>– Teen activity programs</li>
-        </ul>
+        <div>
+          <h2>Features</h2>
+          <ul>
+            <li>– Fine dining restaurants </li>
+            <li>– Casual dining options</li>
+            <li>– Buffet or breakfast areas</li>
+            <li>– Rooftop bars and lounges</li>
+            <li>– Childcare or babysitting services</li>
+            <li>– Kids’ clubs and play areas</li>
+            <li>– Family-friendly room options</li>
+            <li>– Teen activity programs</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
